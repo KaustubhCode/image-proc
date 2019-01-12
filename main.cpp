c5bd09d193890245d863923d1dac597fcfa912c4
Aby <abhay.saxena003@gmail.com>
2019-01-12 16:52:06 +0530

    Header files and conv function added in main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include "conv.h"
 using namespace std;
 
 bool debug = true;
@@ -134,7 +135,24 @@ int main(int argc, char** argv)
 	    cout << "Error!";
 	    exit(0);
 	}
+
 	// Convolution Operation
+
+	int image[6][6]={{3,0,1,2,7,4},
+					{1,5,8,9,3,1},
+					{2,7,2,5,1,3},
+					{0,1,3,1,7,8},
+					{4,2,1,6,2,8},
+					{2,4,5,2,3,8}};
+
+	int ker[3][3] ={{-1,0,1},
+					{-1,0,1},
+					{-1,0,1}};
+
+	int ker2[1][1] = {{2}};
+
+
+	conv((int *)image,(int *)ker,6,3);
 	return 0;
 }
 
